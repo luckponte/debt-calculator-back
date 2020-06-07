90be93c32211fe0a6da1acdb46c8587c7e63d6e2
luckponte <luckpontecorreia@gmail.com>
2020-06-07 19:25:44 -0300

    Clear unused exposed endpoints

--- Controllers/DebtsController.cs
+++ Controllers/DebtsController.cs
@@ -62,67 +62,7 @@ namespace debt_calculator_api.Controllers
 
             return Ok(debts);
         }
-
-        // PUT: api/Debts/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutDebts(int id, Debts debts)
-        {
-            if (id != debts.debtId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(debts).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DebtsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Debts
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Debts>> PostDebts(Debts debts)
-        {
-            _context.Debts.Add(debts);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetDebts", new { id = debts.debtId }, debts);
-        }
-
-        // DELETE: api/Debts/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Debts>> DeleteDebts(int id)
-        {
-            var debts = await _context.Debts.FindAsync(id);
-            if (debts == null)
-            {
-                return NotFound();
-            }
-
-            _context.Debts.Remove(debts);
-            await _context.SaveChangesAsync();
-
-            return debts;
-        }
-
+        
         private bool DebtsExists(int id)
         {
             return _context.Debts.Any(e => e.debtId == id);
