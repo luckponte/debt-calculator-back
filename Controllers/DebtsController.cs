b497d26d71aa6fa0a51c54a7d5db2664ecb4ffaf
luckponte <luckpontecorreia@gmail.com>
2020-06-07 19:15:05 -0300

    Add calculations

--- Controllers/DebtsController.cs
+++ Controllers/DebtsController.cs
@@ -24,16 +24,43 @@ namespace debt_calculator_api.Controllers
 
         // GET: api/Debts/5
         [HttpGet("{userId}")]
-        public async Task<ActionResult<IEnumerable<Debts>>> GetDebts(int userId)
+        public async Task<IActionResult> GetDebts(int userId)
         {
-            var debts = await _context.Debts.Where(d=> d.userId == userId).ToListAsync();
+            var debts = await (
+                from d in _context.Debts 
+                select new CalculatedDebts
+                {
+                    debtId = d.debtId,
+                    deadlineDate = d.deadlineDate,
+                    debtValue = d.debtValue,
+                    phone = d.phone,
+                    userId = d.userId,
+                }).Where(d=> d.userId == userId).ToListAsync();
+            var conf = await _context.Configs.FirstAsync();
 
             if (debts == null)
             {
                 return NotFound();
             }
 
-            return debts;
+            // Realiza os cálculos de atualização da dívida
+            long delay;
+            foreach (var debt in debts)
+            {
+                debt.maxParcels = conf.maxParcels;
+
+                delay = ((long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds) - debt.deadlineDate;
+
+                if(delay <= 0)
+                {
+                    delay = 0;
+                }
+
+                debt.interestValue = (1 + conf.interestRate) * ( delay/86400000 );
+                debt.finalValue = debt.interestValue + debt.debtValue;
+            }
+
+            return Ok(debts);
         }
 
         // PUT: api/Debts/5
--- Models/Debts.cs
+++ Models/Debts.cs
@@ -11,9 +11,17 @@ namespace debt_calculator_api.Models
         public long deadlineDate { get; set; }
         [Required(ErrorMessage="Campo obrigatório")]
         public double debtValue { get; set; }
+        [Required(ErrorMessage="Campo obrigatório")]
         public string phone {get; set;}
 
         public int userId { get; set; }
         public Users User { get; set; }
+    }
+
+    public class CalculatedDebts : Debts
+    {
+        public int maxParcels { get; set; }
+        public double interestValue { get; set; }
+        public double finalValue { get; set; }
     }
 }
